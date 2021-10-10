c40635d82de8c31076e1954cc7f1a344b445aad3
romeueh <75344382+romeueh@users.noreply.github.com>
2021-10-10 16:21:03 -0400

    Delete chp05 directory

--- chp05/overview/short2.cpp
+++
@@ -1,6 +0,0 @@
-// A really useless program, but a program nevertheless
-
-int main(int argc, char *argv[]){
-
-   return 0;
-}
--- chp05/overview/void.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(){
-   int a = 5;
-   void *p = &a;
-   printf("The pointer p points at address %p and value %d\n", p, *((int *)p));
-   return 0;
-}
