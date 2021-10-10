bb0cf63b80b1ea9d0583b28dff293e81b9eb0a58
romeueh <75344382+romeueh@users.noreply.github.com>
2021-10-10 16:21:45 -0400

    Delete chp09 directory

--- chp09/makefiles/test.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-   int x = 1, y = 5;
-   cout << "z = x + y = " << x+y << endl;
-   return 0;
-}
--- chp09/makefiles2/hello.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-   cout << "Hello from the Beagle board!" << endl;
-   return 0;
-}
